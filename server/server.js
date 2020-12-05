dafeca22dde4ba04cfa9541765122b175d439bf0
akshaypatel99 <akshaypatel99@gmail.com>
2020-12-05 10:54:12 +0000

    Server file & compile error in Tooltip fixed

--- server/server.js
+++ server/server.js
@@ -1,16 +1,21 @@
 const path = require('path');
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, '/build')));
 
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
-});
+	app.get('*', (req, res, next) =>
+		res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res, next) => {
+		res.send('API is running...');
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
